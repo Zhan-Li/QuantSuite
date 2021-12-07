3164f2e5a5da9477ae36a56117af9fba1f57ac36
Zhan Li <zhan.li@aol.com>
2021-12-06 21:57:11 -0600

    rename

---
+++ data_processor/IBES.py
@@ -0,0 +1,14 @@
+import pyspark.sql.functions as f
+from data_processor.common import *
+
+usr, pin = get_mysql_secret()
+spark = init_spark()
+IBES_file = 'data/statsum_epsus.sas7bdat'
+IBES = sas_to_parquet(spark, IBES_file)\
+    .withColumn('ue', (f.col('actual') - f.col('meanest')))\
+    .withColumn('sue', f.col('ue') /f.col('stdev'))\
+    .select('cusip', 'statpers', 'anndats_act', 'sue')
+write_to_mysql(IBES, 'localhost/ibes', 'ibes', usr, pin)
+
+
+
