6299828586fb9a1fb8cef3ff3953b5acac8cb189
Zhan Li <zhan.li@aol.com>
2021-12-07 09:55:34 -0600

    add algo trading examples

---
+++ examples/algo_binance/main_IB.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import xmltodict
+from yahoo_earnings_calendar import YahooEarningsCalendar
+from datetime import date, timedelta
+import importlib
+import algo
+importlib.reload(algo)
+from algo import Algo
+import numpy as np
+from ib_insync import IB, util, IBC
+import json
+import datetime
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.expand_frame_repr', False)
+# start IB
+
+ibc = IBC(983, tradingMode='live')
+ibc.start()
+
+# connect IB-----------------------------------------
+ib = IB()
+ib.connect('127.0.0.1', 1983, clientId=1)
+# download hist_volume ------------------------------
+algo = Algo(ib)
+#algo.save_avg_hist_volume()
+# trade ------------------------------
+now = datetime.datetime.now()
+start_time = now.replace(hour=14, minute=45, second=0, microsecond=0)
+open_orders = True
+while bool(open_orders) is True and now > start_time:
+    algo.sell_current_positions()
+    open_orders = ib.openOrders()
+    if len(open_orders) == 0:
+        algo.buy_target_positions()
+
+# report --------------
+algo.report()
+
