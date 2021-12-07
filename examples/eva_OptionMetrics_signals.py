b4fbd0ffcdf8680c0e6bf8a9be7e950f7c424570
Zhan Li <zhan.li@aol.com>
2021-12-06 22:12:36 -0600

    move eva and project files to examples folder

---
+++ examples/eva_OptionMetrics_signals.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import pyspark.sql.functions as f
+from pyspark import StorageLevel
+import importlib
+import class_sig_option
+importlib.reload(class_sig_option)
+from class_sig_option import OptionSignal
+from pyspark.conf import SparkConf
+from pyspark.sql import SparkSession, Window, DataFrame
+import class_sig_evaluator; importlib.reload(class_sig_evaluator)
+import funcs_utilities as utils
+import class_sig_technical; importlib.reload(class_sig_technical)
+import numpy as np
+from class_sig_technical import TechnicalSignal
+
+import class_data_manipulator
+importlib.reload(class_data_manipulator)
+from class_data_manipulator import DataManipulator
+from pandas_profiling import ProfileReport
+import matplotlib.pyplot as plt
+import datetime
+import json
+
+# pandas option
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.expand_frame_repr', False)
+# read SQL credentials
+with open('secret.json') as myfile:
+    secrets = json.load(myfile)
+usr = secrets['mysql_usr']
+pin= secrets['mysql_password']
+# database write function
+def write_to_mysql(data, database, table):
+    data.write.format('jdbc').options(
+      url=f'jdbc:mysql://localhost/{database}',
+      driver='com.mysql.cj.jdbc.Driver',
+      dbtable=table,
+      user=usr,
+      password=pin).mode('overwrite').save()
+# setup spark
+spark_conf = SparkConf() \
+        .set("spark.executor.memory", '50g') \
+        .set("spark.driver.memory", "50g") \
+        .set('spark.driver.maxResultSize', '0')\
+        .set('spark.driver.extraJavaOptions', '-Duser.timezone=UTC') \
+        .set('spark.executor.extraJavaOptions', '-Duser.timezone=UTC')
+spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+# prepare data
+sec = spark.read.parquet('data/dsf_linked').filter(f.col('date') >= '1996-01-01')
+
+sec = TechnicalSignal(sec, 'date', 'cusip')\
+        .add_std('ret', -20, 0, 'ret_std').data
+opt = spark.read.parquet('data/opprcd_1996_2020') \
+    .filter(f.col('impl_volatility').isNotNull())\
+    .join(sec, on=['date', 'cusip'])\
+    .withColumn('ks', f.col('strike_price')/f.col('prc')) \
+    .withColumn('p_ratio', f.col('opt_prc') / f.col('prc')) \
+    .withColumn('rv-iv', f.col('ret_std') - f.col('impl_volatility'))\
+    .withColumn('elasticity', f.col('delta') * f.col('prc') / f.col('opt_prc'))\
+    .select('date', 'cusip', 'cp_flag', 'opt_vol_adj', 'opt_vold', 'open_interest_adj', 'oi_adj_change',
+            'vol_adj', 'vol_d', 'opt_prc', 'prc', 'p_ratio', 'ks', 'exdays', 'elasticity', 'impl_volatility', 'rv-iv')
+#utils.profile_data(opt)
+
+
+## osv signal
+print('OSV' + '-'*100)
+sigs = []
+for opt_vol in ['opt_vol_adj', 'opt_vold', 'open_interest_adj', 'oi_adj_change']:
+    for sec_vol in ['vol_adj', 'vol_d']:
+            opt = opt.withColumn('v_ratio', f.col(opt_vol)/f.col(sec_vol))
+            sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=None)\
+                .gen_os_v('v_ratio', f'osv|{opt_vol}|{sec_vol}')
+            sigs.append(sig)
+osv = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(osv, 'osv')
+## osp signal
+print('osp' + '-'*100)
+sigs = []
+for weight in [None,  'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+            sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight)\
+                .gen_os_p('p_ratio', f'osp|{weight}')
+            sigs.append(sig)
+osp = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(osp, 'osp')
+## volume signal
+print('vol' + '-'*100)
+sigs = []
+for vol in ['opt_vol_adj', 'opt_vold', 'open_interest_adj', 'oi_adj_change']:
+            sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P',weight=None)\
+                .gen_vol(vol, f'vol|{vol}')
+            sigs.append(sig)
+vol = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(vol, 'vol')
+## ks signal
+print('ks' + '-'*100)
+sigs = []
+for weight in [None, 'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+    sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight) \
+        .gen_ks('ks', f'ks|{weight}')
+    sigs.append(sig)
+ks = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(ks, 'ks')
+## exdays signal
+print('exdays' + '-'*100)
+sigs = []
+for weight in [None, 'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+    sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight) \
+        .gen_expdays('exdays', f'exdays|{weight}')
+    sigs.append(sig)
+exdays = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(exdays, 'exdays')
+## elasticity signal
+print('elasticity' + '-'*100)
+sigs = []
+for weight in [None,'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+                sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight)\
+                    .gen_elasticity('elasticity', f'elasticity|{weight}')
+                sigs.append(sig)
+elasticity = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(elasticity, 'elasticity')
+## IV spread signal
+print('IV spread' + '-'*100)
+sigs = []
+for weight in [None,'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+                sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight)\
+                    .gen_IV('impl_volatility', f'IV|{weight}')
+                sigs.append(sig)
+IV_spread = utils.multi_join(sigs, on=['date', 'cusip'])
+utils.profile_data(IV_spread, file_name='IV_spread.html')
+write_to_mysql(IV_spread, 'iv_spread')
+## IV skew
+print('IV skew' + '-'*100)
+sigs = []
+for weight in [None, 'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+                sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight)\
+                    .gen_IV_skew('impl_volatility', 'ks',  f'IV_skew|{weight}')
+                sigs.append(sig)
+IV_skew = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(IV_skew, 'iv_skew')
+## rv-iv
+print('rv-iv' + '-'*100)
+sigs = []
+for weight in [None, 'opt_vol_adj', 'opt_vold', 'open_interest_adj']:
+                sig = OptionSignal(opt, 'date', 'cusip', 'cp_flag', 'C', 'P', weight=weight)\
+                    .gen_rv_iv_spread('rv-iv', f'rv-iv|{weight}')
+                sigs.append(sig)
+RV_IV = utils.multi_join(sigs, on=['date', 'cusip'])
+write_to_mysql(RV_IV, 'rv_iv')
---
+++ examples/proj_technical_signals.py
@@ -0,0 +1,52 @@
+import importlib
+from pyspark.conf import SparkConf
+from pyspark.sql import SparkSession
+import src.data_manipulator; importlib.reload(class_data_manipulator)
+import class_sig_technical; importlib.reload(class_sig_technical)
+from class_sig_technical import TechnicalSignal
+from class_data_manipulator import DataManipulator
+import  pyspark.sql.functions as f
+
+spark_conf = SparkConf() \
+        .set("spark.executor.memory", '50g') \
+        .set("spark.driver.memory", '50g') \
+        .set('spark.driver.extraJavaOptions', '-Duser.timezone=UTC') \
+        .set('spark.executor.extraJavaOptions', '-Duser.timezone=UTC')
+        #.set("spark.sql.execution.arrow.pyspark.enabled", "true")\
+        #.set("spark.driver.maxResultSize", "32g") \
+        #.set("spark.memory.offHeap.size", "16g")
+        #.set('spark.sql.execution.arrow.maxRecordsPerBatch', 50000)
+        #.set("spark.jars.packages", "saurfang:spark-sas7bdat:3.0.0-s_2.12") \
+spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
+
+dsf = spark.read.parquet('data/dsf_linked').filter(f.col('date') >= '2006-01-01')
+ts = TechnicalSignal(dsf, 'date', 'cusip')
+dsf_sigs = ts\
+    .add_cumr('ret', 1, 1, 'forward_r1d')\
+    .add_cumr('ret', 1, 5, 'forward_r1w')\
+    .add_cumr('ret', 1, 21, 'forward_r1m')\
+    .add_cumr('ret', 1, 126, 'forward_r6m')\
+    .add_cumr('ret', 1, 252, 'forward_r12m')\
+    .add_cumr('ret', 1, 1, 'forward_r1')\
+    .add_cumr('ret', -252, -21, 'r11m')\
+    .add_cumr('ret',  -147, -21, 'r6m')\
+    .add_cumr('ret', -21, 0,  'r1m') \
+    .add_52w_high('prc_adj', 'high_52w')\
+    .add_std('ret', -21, 0, 'r_std' ) \
+    .add_avg_r('ret', -21, 0, 'r_avg')\
+    .add_turnover('vol', 'shrout', 'turnover_daily')\
+    .add_illiq('ret', 'vol_d', 'illiq_daily') \
+    .add_max_min_r('ret', -21, 0, 'max_r_1m', 'min_r_1m' )\
+    .add_spread('bid', 'ask', 'spread_daily').data
+
+dsf_sigs = DataManipulator(dsf_sigs, 'date', 'cusip')\
+    .add_avg('turnover_daily', -126, 0, 'turnover_6m_avg')\
+    .add_rsd('turnover_daily', -126, 0, 'turnover_6m_rsd')\
+    .add_avg('vol_d', -126, 0, 'vol_d_6m_avg') \
+    .add_rsd('vol_d', -126, 0, 'vol_d_6m_rsd')\
+    .add_avg('illiq_daily', -126, 0, 'illiq_6m_avg') \
+    .add_skewness('ret', -21, 0, 'skewness_1m')\
+    .add_kurtosis('ret', -21, 0, 'kurtosis_1m')\
+    .add_avg('spread_daily', -21, 0, 'spread_1m').data
+
+dsf_sigs.write.mode('overwrite').parquet('tech_signals')
