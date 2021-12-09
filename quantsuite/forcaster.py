02ac15817c63aa10452b712aee805bdab8b4b930
Zhan Li <zhan.li@aol.com>
2021-12-08 22:30:14 -0600

    Update forcaster.py
    merge predict function into backtest function

--- quantsuite/forcaster.py
+++ quantsuite/forcaster.py
@@ -1,20 +1,24 @@
-import pandas as pd
-from tpot import TPOTRegressor
-from pandas import DataFrame
-from sklearn.inspection import PartialDependenceDisplay, permutation_importance
+import pickle
+from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
-from sqlalchemy import MetaData, Table
-from typing import List
-from ray.tune.sklearn import TuneSearchCV
-from sklearn.metrics import mean_squared_error
+import pandas as pd
 import pyspark.sql.functions as f
+from pandas import DataFrame
+from ray.tune.sklearn import TuneSearchCV
+from sklearn.inspection import PartialDependenceDisplay, permutation_importance
+from sklearn.metrics import mean_squared_error
+from sqlalchemy import MetaData, Table
+from tpot import TPOTRegressor
 
-from invtools import PerformanceEvaluation, PortfolioAnalysis
+from quantsuite import PerformanceEvaluation, PortfolioAnalysis
 
 
 class ReturnForecaster:
+    """
+    Wrapper around sklearn and tpot, tailored for time-series return prediction.
+    """
     def __init__(self, data):
         """
         'Data needs to have time index and only contains y and x variables.'
@@ -88,7 +92,9 @@ class ReturnForecaster:
         self.best_params = search.best_params_
         self.best_pipeline = search.best_estimator_
 
-    def autoML_tpot(self, x_train, y_train, cv, use_gpu=True, generations=100, max_time_mins = None,  max_eval_time_mins=30):
+    def autoML_tpot(self, x_train, y_train, cv, use_gpu=True, generations=100, max_time_mins=None,
+                    max_eval_time_mins=30, save_pipeline=False, file_name=None):
+        """auto-ml with tpot"""
         if use_gpu is True:
             config_dict = 'TPOT cuML'
             n_jobs = 1
@@ -98,18 +104,21 @@ class ReturnForecaster:
             print("Warning, you're not using GPU.")
 
         tpot = TPOTRegressor(generations=generations, population_size=100, scoring='neg_mean_squared_error',
-                                 verbosity=2,
-                             cv=cv, n_jobs=n_jobs, max_time_mins = max_time_mins,
-                             max_eval_time_mins = max_eval_time_mins, use_dask = True,
-                             early_stop=10, memory= 'auto', random_state=0, config_dict=config_dict)
+                             verbosity=2,
+                             cv=cv, n_jobs=n_jobs, max_time_mins=max_time_mins,
+                             max_eval_time_mins=max_eval_time_mins, use_dask=True,
+                             early_stop=10, memory='auto', random_state=0, config_dict=config_dict)
         print('Start autoML with Tpot...')
         tpot.fit(x_train.values, y_train.values.reshape(-1, ))
-        tpot.export('fitted.py')
-        self.best_pipeline =  tpot.fitted_pipeline_
-
+        if save_pipeline:
+            if file_name is not None:
+                tpot.export('fitted.py')
+            else:
+                raise Exception('File name not given for the exporeted pipeline')
+        return tpot.fitted_pipeline_
 
     def interpret(self, X, y, feature_names, n_repeats):
-
+        """model interpretation with partial dependence and permutation importance"""
         def plot_partial_dependence():
             fig, ax = plt.subplots(figsize=(12, len(feature_names) // 3 * 4))
             PartialDependenceDisplay.from_estimator(self.best_pipeline, X, range(len(feature_names)),
@@ -129,29 +138,40 @@ class ReturnForecaster:
 
         return plot_partial_dependence(), plot_permutation_importance()
 
-    def predict(self, X, y, cv):
-        print('Predicting on the test set...')
-        if self.best_pipeline is None:
-            raise Exception('best_estimator is None!')
-        self.y_test_true = pd.concat([y.iloc[val_index] for train_index, val_index in cv])
+    def backtest(self, spark, X, y, cv, time:str, freq: str, model = None):
+        """
+        backtest equal weighted long-short portfolio performance on the test dataset
 
-        y_test_pred = [self.best_pipeline.fit(X.iloc[train_index], y.iloc[train_index]).predict(X.iloc[val_index])
-                       for train_index, val_index in cv]
+        Parameters
+        ----------
+        spark: pyspark object
+        X : features
+        y : target
+        cv : index for train, predict.
+        model : model has higher priority than self.best_pipeline. That is, If model and fitted_best_pipeline are both
+            supplied, fitted_best pipeline will be used instead. If model is None, then fitted best_pipeline
+            will be used. Note that best_pipeline from automl_tpot cannot be used because tpot pipeline does not include
+            tranformers.
+
+        Returns
+        -------
+
+        """
+        print('Predicting on the test set...')
+
+        if model:
+            estimator = model
+        elif self.best_pipeline:
+            estimator = self.best_pipeline
+        else:
+            raise Exception('Either model needs to be supplied or best_pipeline needs to provided by search')
+
+        self.y_test_true = pd.concat([y.iloc[predict_index] for train_index, predict_index in cv])
+        y_test_pred = [estimator.fit(X.iloc[train_index], y.iloc[train_index]).predict(X.iloc[predict_index])
+                       for train_index, predict_index in cv]
         self.y_test_pred = pd.Series(np.concatenate(y_test_pred), index=self.y_test_true.index)
         self.test_score = -mean_squared_error(self.y_test_true, self.y_test_pred)
 
-    def search_predict(self, params, model, x_train, y_train, cv_train,
-                       x_test, y_test, cv_test,
-                       n_jobs=-1, use_gpu=False, verbose=2):
-        """
-        convinience function for both search using training data and predict using test data
-        """
-        self.search(params, model, x_train, y_train, cv_train, n_jobs=n_jobs, use_gpu=use_gpu, verbose=verbose)
-        self.predict(x_test, y_test, cv_test)
-
-    def backtest(self, spark, freq, time='date'):
-        if self.y_test_true is None or self.y_test_pred is None:
-            raise Exception('Need to predict on the test data before examining performance.')
         returns = pd.DataFrame({'y_true': self.y_test_true, 'y_pred': self.y_test_pred}).reset_index()
         returns = spark.createDataFrame(returns).withColumn('name', f.lit('None'))
         pa = PortfolioAnalysis(returns, time, 'name', 'y_pred', 'y_true')
@@ -160,6 +180,7 @@ class ReturnForecaster:
         r = r.loc[r['sig_rank'] == 'high_minus_low']['y_true']
         perf = PerformanceEvaluation(r)
         self.perf = perf.get_stats(freq)
+
 
     def insert_to_db(self, dataset_names: List[str], sample_start: str, sample_end: str, sample_freq: str,
                      model_name: str, table: str, db_con):
@@ -187,10 +208,6 @@ class ReturnForecaster:
             [record]
         )
         print(f'Sucessfully inserted records into table {table}')
-
-
-
-
 
 
 def plot_feature_importances(trees, feature_names):
