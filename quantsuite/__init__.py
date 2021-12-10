48dd0ff5130682cc9c7ebc5cdca66410f110daef
Zhan Li <zhan.li@aol.com>
2021-12-09 20:04:49 -0600

    Update __init__.py

--- quantsuite/__init__.py
+++ quantsuite/__init__.py
@@ -1,6 +1,6 @@
 from .amfe import AMFE
 from .data_manipulator import DataManipulator
-from .forcaster import ReturnForecaster
+from .forcaster import ReturnForecaster, CustomCV, TrainTestSplitter
 from .pairs_trading import PairsTrading
 from .performance_evaluation import PerformanceEvaluation
 from .portfolio_analysis import PortfolioAnalysis
