e759056b55267d1f093017df3d53ddca2422e08b
Zhan Li <zhan.li@aol.com>
2021-12-09 20:05:14 -0600

    Update forcaster.py
    create customCV and Traintestsplitter classes

--- quantsuite/forcaster.py
+++ quantsuite/forcaster.py
@@ -12,50 +12,44 @@ from sklearn.metrics import mean_squared_error
 from sqlalchemy import MetaData, Table
 from tpot import TPOTRegressor
 
-from quantsuite import PerformanceEvaluation, PortfolioAnalysis
+from quantsuite.performance_evaluation import PerformanceEvaluation
+from quantsuite.portfolio_analysis import PortfolioAnalysis
 
 
-class ReturnForecaster:
-    """
-    Wrapper around sklearn and tpot, tailored for time-series return prediction.
-    """
-    def __init__(self, data):
-        """
-        'Data needs to have time index and only contains y and x variables.'
-        """
+class TrainTestSplitter:
+    """Split time series into train test datasets"""
+
+    def __init__(self, data: DataFrame):
         self.data = data
-        self.best_params = None
-        self.best_pipeline = None
-        self.best_score = None
-        self.y_test_true = None
-        self.y_test_pred = None
-        self.test_score = None
-        self.perf = None
-        if isinstance(data.index, pd.DatetimeIndex) is not True:
-            raise Exception('Data index is not DatetimeIndex')
+        if not isinstance(data.index, pd.DatetimeIndex):
+            raise Exception('Data index needs to be pd.DatetimeIndex')
 
-    def split_train_test(self, train_size: float, y_var: str):
+    def split(self, train_size: float):
         """
         split train validation data
-        split_time: time to split train and valiadation dataset
+        train_size: fraction to used as train
         """
         index = self.data.index.unique().sort_values()
         cut_date = index[int(train_size * len(index))]
-        train = self.data.loc[self.data.index < cut_date]
-        test = self.data.loc[self.data.index >= cut_date]
-        return train.drop(y_var, axis=1), train[y_var], test.drop(y_var, axis=1), test[y_var]
+        return self.data.loc[self.data.index < cut_date], self.data.loc[self.data.index >= cut_date]
 
-    @staticmethod
-    def gen_cv_index(train: DataFrame, window_size: int, window_step=1, moving_window: bool = True):
+
+class CustomCV:
+    """"""
+
+    def __init__(self, data: DataFrame):
+        self.data = data
+        if not isinstance(data.index, pd.DatetimeIndex):
+            raise Exception('Data index needs to be pd.DatetimeIndex')
+
+    def gen_cv_index(self, window_size: int, window_step=1, moving_window: bool = True):
         """
         customized cross-validation index
         train: training dataset
         window: window size
         """
-        if isinstance(train.index, pd.DatetimeIndex) is not True:
-            raise Exception('Data index is not DatetimeIndex')
-        time = train.index.name
-        train = train.reset_index()
+        time = self.data.index.name
+        train = self.data.reset_index()
         groups = train.groupby(time).groups
         sorted_index = [index.values for (key, index) in sorted(groups.items())]
         counter = range(window_size, len(groups) - window_size - window_step, window_step)
@@ -69,7 +63,25 @@ class ReturnForecaster:
                   for i in counter]
         return cv
 
-    def search(self, params, pipeline, x_train, y_train, cv_train, n_trial, n_jobs=-1, use_gpu=False, verbose=2):
+
+class ReturnForecaster:
+    """
+    Wrapper around sklearn and tpot, tailored for time-series return prediction.
+    """
+
+    def __init__(self, x_train, y_train, cv_train):
+        self.x_train = x_train
+        self.y_train = y_train
+        self.cv_train = cv_train
+        self.best_params = None
+        self.best_pipeline = None
+        self.best_score = None
+        self.y_test_true = None
+        self.y_test_pred = None
+        self.test_score = None
+        self.perf = None
+
+    def search(self, params, pipeline, n_trial, n_jobs=-1, use_gpu=False, verbose=2):
         """
         search hyperparameter with the training and validation data and predict with the test dataset.
         """
@@ -80,21 +92,21 @@ class ReturnForecaster:
             n_trials=n_trial,
             early_stopping=False,
             scoring='neg_mean_squared_error',
-            cv=cv_train,
+            cv=self.cv_train,
             max_iters=1,
             verbose=verbose,
             n_jobs=n_jobs,
             use_gpu=use_gpu,
         )
         print('Searching the the best hyperparameters...')
-        search.fit(x_train, y_train)
+        search.fit(self.x_train, self.y_train)
         self.best_score = search.best_score_
         self.best_params = search.best_params_
         self.best_pipeline = search.best_estimator_
 
-    def autoML_tpot(self, x_train, y_train, cv, use_gpu=True, generations=100, max_time_mins=None,
-                    max_eval_time_mins=30, save_pipeline=False, file_name=None):
-        """auto-ml with tpot"""
+    def autoML_tpot(self, generations=100, max_time_mins=None,
+                    max_eval_time_mins=30, use_gpu=True,  save_pipeline=False, file_name=None):
+        """auto-ml with tpot."""
         if use_gpu is True:
             config_dict = 'TPOT cuML'
             n_jobs = 1
@@ -105,11 +117,14 @@ class ReturnForecaster:
 
         tpot = TPOTRegressor(generations=generations, population_size=100, scoring='neg_mean_squared_error',
                              verbosity=2,
-                             cv=cv, n_jobs=n_jobs, max_time_mins=max_time_mins,
+                             cv=self.cv_train, n_jobs=n_jobs, max_time_mins=max_time_mins,
                              max_eval_time_mins=max_eval_time_mins, use_dask=True,
                              early_stop=10, memory='auto', random_state=0, config_dict=config_dict)
         print('Start autoML with Tpot...')
-        tpot.fit(x_train.values, y_train.values.reshape(-1, ))
+        x_train = self.x_train.values if hasattr(self.x_train, 'values') else self.x_train
+        y_train = self.y_train.values if hasattr(self.y_train, 'values') else self.y_train
+
+        tpot.fit(x_train, y_train.reshape(-1, ))
         if save_pipeline:
             if file_name is not None:
                 tpot.export('fitted.py')
@@ -118,7 +133,8 @@ class ReturnForecaster:
         return tpot.fitted_pipeline_
 
     def interpret(self, X, y, feature_names, n_repeats):
-        """model interpretation with partial dependence and permutation importance"""
+        """model interpretation with partial dependence and permutation importance."""
+
         def plot_partial_dependence():
             fig, ax = plt.subplots(figsize=(12, len(feature_names) // 3 * 4))
             PartialDependenceDisplay.from_estimator(self.best_pipeline, X, range(len(feature_names)),
@@ -138,9 +154,9 @@ class ReturnForecaster:
 
         return plot_partial_dependence(), plot_permutation_importance()
 
-    def backtest(self, spark, X, y, cv, time:str, freq: str, model = None):
+    def backtest(self, spark, X, y, cv, time: str, freq: str, model=None):
         """
-        backtest equal weighted long-short portfolio performance on the test dataset
+        backtest equal weighted long-short portfolio performance on the test dataset.
 
         Parameters
         ----------
@@ -181,20 +197,19 @@ class ReturnForecaster:
         perf = PerformanceEvaluation(r)
         self.perf = perf.get_stats(freq)
 
-
     def insert_to_db(self, dataset_names: List[str], sample_start: str, sample_end: str, sample_freq: str,
                      model_name: str, table: str, db_con):
         """
-        Insert search result and performance statistics into dataset
+        Insert search result and performance statistics into dataset.
         """
-        if None in [self.best_score, self.best_params, self.pipeline, self.test_score, self.perf]:
+        if None in [self.best_score, self.best_params, self.best_pipeline, self.test_score, self.perf]:
             raise Exception('Need to search the the best hyperparameters/examine performance before insertion.')
         record = {'dataset_name': ','.join(dataset_names), 'sample_start': sample_start, 'sample_end': sample_end,
                   'sample_freq': sample_freq, 'model_name': model_name,
                   'best_val_score': self.best_score, 'test_score': self.test_score,
                   'best_params': self.best_params,
                   'perf': self.perf,
-                  'best_model': pickle.dumps(self.pipeline)}
+                  'best_model': pickle.dumps(self.best_pipeline)}
         dtypes = ['VARCHAR(100)', 'VARCHAR(20)', 'VARCHAR(20)', 'VARCHAR(10)', 'VARCHAR(60)', 'DOUBLE', 'DOUBLE',
                   'JSON', 'JSON', 'LONGBLOB']
 
