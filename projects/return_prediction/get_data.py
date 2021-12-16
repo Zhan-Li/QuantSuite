75705ac8a7e7d19bea3feccbca7387547bc55ad1
Zhan Li <zhan.li@aol.com>
2021-12-15 22:12:04 -0600

    Update get_data.py

--- projects/return_prediction/get_data.py
+++ projects/return_prediction/get_data.py
@@ -6,15 +6,16 @@ with open('secret.json') as myfile:
     secrets = json.load(myfile)
 usr = secrets['mysql_usr']
 pin= secrets['mysql_password']
+start = '2008-01-01'
 # import option signals from mysql
 optionsig_connection = create_engine(f'mysql+pymysql://{usr}:{pin}@localhost/option_sig')
 dfs = []
 for table in optionsig_connection.table_names():
-    df = pd.read_sql(f"SELECT * FROM {table}", con=optionsig_connection)
+    df = pd.read_sql(f"SELECT * FROM {table} WHERE date >='{start}'", con=optionsig_connection)
     dfs.append(df)
 # import crsp from mysql
 crsp_connection = create_engine(f'mysql+pymysql://{usr}:{pin}@localhost/crsp')
-crsp = pd.read_sql(f"SELECT date, cusip, ret FROM stock", con=crsp_connection)
+crsp = pd.read_sql(f"SELECT date, cusip, mktcap, ret FROM stock WHERE date >='{start}'", con=crsp_connection)
 # combine data
 crsp = crsp.sort_values('date')
 stock = crsp
