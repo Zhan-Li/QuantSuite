c5a79c8a95d453e1a4fe064c048c73d269623b2b
Zhan Li <zhan.li@aol.com>
2021-12-19 22:07:49 -0600

    add an yahoo finance option scraper

---
+++ quantsuite/downloader/options.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import pandas as pd
+import time
+
+
+def scrape_options(ticker: str, min_exp_date=None, max_exp_date=None, pause=1):
+    """
+    Download option date from yahoo finance
+    pause: pause between downloading options of different expiration dates
+    """
+    stock = yf.Ticker(ticker)
+    try:
+        options = pd.DataFrame()
+        for exp_date in stock.options:
+            if min_exp_date is not None and exp_date < min_exp_date:
+                break
+            if max_exp_date is not None and exp_date > max_exp_date:
+                break
+            print(f'Downloading {ticker} options with expiration date {exp_date}')
+            opt_chain = stock.option_chain(exp_date)
+            puts = opt_chain.puts
+            puts['type'] = 'put'
+            puts['expiration'] = exp_date
+            options = options.append(puts)
+            calls = opt_chain.calls
+            calls['type'] = 'call'
+            calls['expiration'] = exp_date
+            options = options.append(calls)
+            time.sleep(pause)
+        return options
+    except IndexError as e:
+        print(e, ticker, 'has no options')
