33471eace39dcfa8fda37dd15692b928f6a667e7
Zhan Li <zhan.li@aol.com>
2021-12-30 10:32:08 -0600

    Update scorer.py

--- quantsuite/forcaster/scorer.py
+++ quantsuite/forcaster/scorer.py
@@ -4,20 +4,10 @@ custom scoring functions.
 from sklearn.metrics import make_scorer
 import pandas as pd
 import numpy as np
+from quantsuite import get_port_r
 
-
-def _sharpe_ratio(r_true, r_pred):
-    def get_port_r(r_true: pd.Series, r_pred: pd.Series):
-        if not isinstance(r_true.index, pd.DatetimeIndex):
-            raise Exception('r_true needs to have datatime index')
-        returns = r_true.to_frame(name='r_true')
-        returns['r_pred'] = r_pred
-        returns['r_pred_pct'] = returns['r_pred'].groupby(returns.index).rank(method='first', pct=True)
-        return returns.groupby(returns.index) \
-            .apply(
-            lambda x: x.loc[x['r_pred_pct'] >= 0.9]['r_true'].mean() - x.loc[x['r_pred_pct'] <= 0.1]['r_true'].mean())
-
-    port_r = get_port_r(r_true, r_pred)
+def _sharpe_ratio(r_true: pd.Series, r_pred: pd.Series):
+    port_r = get_port_r(r_true, r_pred, ntile=10)
     return port_r.mean() / port_r.std()
 
 
@@ -26,7 +16,7 @@ def _IC(r_true, r_pred):
     if cov[0,0] == 0 or cov[1,1] == 0:
         return -2
     else:
-        return cov[0,1]/(cov[0,0] * cov[1,1])
+        return cov[0,1]/np.sqrt(cov[0,0] * cov[1,1])
 
 
 sharpe_ratio = make_scorer(_sharpe_ratio, greater_is_better=True)
