d1a3c91208f9a21df3eaa91a2ede0fbb42afd2a9
Zhan Li <zhan.li@aol.com>
2021-12-30 10:31:44 -0600

    Update portfolio_analysis.py

--- quantsuite/portfolio_analysis.py
+++ quantsuite/portfolio_analysis.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 import importlib
-import invtools.misc_funcs as utils
+import quantsuite.misc_funcs as utils
+
 importlib.reload(utils)
 import pyspark.sql.functions as f
 from pyspark.sql import DataFrame as PysparkDataFrame
@@ -14,19 +15,31 @@ from typing import List
 import re
 
 
+def get_port_r(r_true: pd.Series, r_pred: pd.Series, ntile=10):
+    if not isinstance(r_true.index, pd.DatetimeIndex):
+        raise Exception('r_true needs to have datatime index')
+    returns = r_true.to_frame(name='r_true')
+    returns['r_pred'] = r_pred
+    returns['r_pred_pct'] = returns['r_pred'].groupby(returns.index).rank(method='first', pct=True)
+    return returns.groupby(returns.index) \
+        .apply(
+        lambda x: x.loc[x['r_pred_pct'] >= 1 - 1 / ntile]['r_true'].mean() - x.loc[x['r_pred_pct'] <= 1 / ntile][
+            'r_true'].mean())
+
+
 class PortfolioAnalysis:
     """
     data: pyspark dataframe with security returns and signals
     """
 
-    def __init__(self, data: PysparkDataFrame, time: str, name: str, sig: str, forward_r:str,
+    def __init__(self, data: PysparkDataFrame, time: str, name: str, sig: str, forward_r: str,
                  weight=None, var_list=None):
         """
         r: forward return
         """
         self.time, self.r, self.sig, self.name = time, forward_r, sig, name
-        #data = data[[time, name, sig, forward_r, weight]] if vw is True else data[[time, name, sig, r]]
-        #self.data = data.loc[data[self.sig].notnull()].loc[data[self.sig] != np.nan]
+        # data = data[[time, name, sig, forward_r, weight]] if vw is True else data[[time, name, sig, r]]
+        # self.data = data.loc[data[self.sig].notnull()].loc[data[self.sig] != np.nan]
 
         self.data = data.filter(f.col(self.sig).isNotNull()).filter(f.col(self.sig) != np.nan)
         if self.data.limit(1).count == 0:
@@ -41,13 +54,13 @@ class PortfolioAnalysis:
             self.var_list = [sig]
         else:
             self.var_list = var_list + [sig]
-        #self.symbols_all = self.data[self.name].unique().tolist()
+        # self.symbols_all = self.data[self.name].unique().tolist()
         self.portr = None
 
     def get_IC(self):
         return self.data.agg(f.corr(self.r, self.sig).alias('correlation'))
 
-    def gen_portr(self, shift:int, ntile=None, sort_var2=None, ntile2=None, dependent_sort= True,
+    def gen_portr(self, shift: int, ntile=None, sort_var2=None, ntile2=None, dependent_sort=True,
                   total_position=0, commission=0, turnover=0) -> PandasDataFrame:
         """
         ntile: n quantile portfolio formation
@@ -61,8 +74,8 @@ class PortfolioAnalysis:
         if sort_var2 is not None and ntile2 is not None:
             self.data = self.data.filter(f.col(sort_var2).isNotNull()).filter(f.col(sort_var2) != np.nan)
             cond_ranked = self.data.withColumn('cond_rank',
-                                       f.ntile(ntile2).over(Window.partitionBy(self.time).orderBy(sort_var2)))\
-                            .withColumn('cond_rank', f.col('cond_rank').cast(IntegerType()))
+                                               f.ntile(ntile2).over(Window.partitionBy(self.time).orderBy(sort_var2))) \
+                .withColumn('cond_rank', f.col('cond_rank').cast(IntegerType()))
             sig_group = [self.time, 'cond_rank'] if dependent_sort is True else self.time
             port_group = [self.time, 'sig_rank', 'cond_rank']
             merge_vars = [self.time, 'cond_rank']
@@ -73,7 +86,7 @@ class PortfolioAnalysis:
             merge_vars = [self.time]
         # generate sig ranks
         sig_ranked = cond_ranked \
-            .withColumn('sig_rank', f.ntile(ntile).over(Window.partitionBy(sig_group).orderBy(self.sig)))\
+            .withColumn('sig_rank', f.ntile(ntile).over(Window.partitionBy(sig_group).orderBy(self.sig))) \
             .withColumn('sig_rank', f.col('sig_rank').cast(IntegerType()))
         # generate portfolio returns
         agged = sig_ranked.groupBy(port_group) \
@@ -82,35 +95,35 @@ class PortfolioAnalysis:
                  f.count(self.r).alias('n_assets'),
                  *[f.mean(x).alias(x) for x in self.var_list])
         # generate high minus low
-        high = agged.filter(f.col('sig_rank') == ntile)\
-            .withColumnRenamed(self.r, 'r_h')\
+        high = agged.filter(f.col('sig_rank') == ntile) \
+            .withColumnRenamed(self.r, 'r_h') \
             .withColumnRenamed('n_assets', 'n_assets_h') \
-            .withColumnRenamed(self.name, self.name+'_h') \
-            .select(merge_vars + ['r_h', 'n_assets_h', self.name+'_h'])
+            .withColumnRenamed(self.name, self.name + '_h') \
+            .select(merge_vars + ['r_h', 'n_assets_h', self.name + '_h'])
         low = agged.filter(f.col('sig_rank') == 1) \
             .withColumnRenamed(self.r, 'r_l') \
             .withColumnRenamed('n_assets', 'n_assets_l') \
             .withColumnRenamed(self.name, self.name + '_l') \
-            .select(merge_vars + ['r_l', 'n_assets_l', self.name+'_l'])
+            .select(merge_vars + ['r_l', 'n_assets_l', self.name + '_l'])
         high_minus_low = high.join(low, on=merge_vars) \
-            .withColumn('sig_rank', f.lit('high_minus_low'))\
-            .withColumn(self.r, f.col('r_h')-f.col('r_l')) \
-            .withColumn('n_assets', f.col('n_assets_h')+f.col('n_assets_l'))\
+            .withColumn('sig_rank', f.lit('high_minus_low')) \
+            .withColumn(self.r, f.col('r_h') - f.col('r_l')) \
+            .withColumn('n_assets', f.col('n_assets_h') + f.col('n_assets_l')) \
             .drop('r_h', 'r_l', 'n_assets_h', 'n_assets_l')
         # combine high minus low with ranked data
-        self.portr = agged\
-            .sort('sig_rank')\
+        self.portr = agged \
+            .sort('sig_rank') \
             .withColumn('sig_rank', f.col('sig_rank').cast(StringType())) \
-            .unionByName(high_minus_low, allowMissingColumns=True)\
-            .withColumn('trading_cost', 2*f.col('n_assets')*turnover*commission/total_position)\
-            .toPandas()\
-            .sort_values(self.time)\
+            .unionByName(high_minus_low, allowMissingColumns=True) \
+            .withColumn('trading_cost', 2 * f.col('n_assets') * turnover * commission / total_position) \
+            .toPandas() \
+            .sort_values(self.time) \
             .set_index(self.time)
         self.portr['port_r'] = self.portr.groupby(['sig_rank', 'cond_rank'])[self.r].shift(shift) \
             if 'cond_rank' in self.portr.columns else self.portr.groupby(['sig_rank'])[self.r].shift(shift)
         return self.portr
 
-    def sort_return(self, days=1, average_across_2nd_ranks = False, trading_cost=False) -> PandasDataFrame:
+    def sort_return(self, days=1, average_across_2nd_ranks=False, trading_cost=False) -> PandasDataFrame:
         """
         data: data generated by method bivariate_portfolio_r()
         conditional portfolio
@@ -122,7 +135,7 @@ class PortfolioAnalysis:
 
         def describe(df):
             df = df[::days]
-            r = (df['port_r']-df['trading_cost']) if trading_cost is True else df['port_r']
+            r = (df['port_r'] - df['trading_cost']) if trading_cost is True else df['port_r']
             d = {}
             d['mean_return'] = r.mean()
             d['t_statistic'] = ttest_1samp(r, 0).statistic
@@ -131,13 +144,13 @@ class PortfolioAnalysis:
             d['annual_return'] = (1 + r.mean()) ** (252 / days) - 1
             d['n_assets'] = df['n_assets'].mean()
             for var in self.var_list:
-                d[var+'_mean'] = df[var].mean()
+                d[var + '_mean'] = df[var].mean()
             return pd.Series(d)
 
         returns = self.portr.loc[self.portr['port_r'].notnull()]
         if 'cond_rank' in self.portr.columns:
             if average_across_2nd_ranks is True:
-                returns = returns.drop('cond_rank', axis = 1).groupby([self.time, 'sig_rank']).mean()
+                returns = returns.drop('cond_rank', axis=1).groupby([self.time, 'sig_rank']).mean()
                 group_vars = ['sig_rank']
             else:
                 group_vars = ['cond_rank', 'sig_rank']
@@ -147,37 +160,37 @@ class PortfolioAnalysis:
 
     def get_transact_cost(self, single_trip_cost: float):
         r = self.portr.loc[self.portr['sig_rank'] == 'high_minus_low']
-        r[self.name+'_h_lag'] = r[self.name+'_h'].shift(1)
+        r[self.name + '_h_lag'] = r[self.name + '_h'].shift(1)
         r[self.name + '_l_lag'] = r[self.name + '_l'].shift(1)
         for index, row in r.iterrows():
             try:
-                turnover_h = len([i for i in row[self.name+'_h_lag'] if i not in row[self.name+'_h']])\
-                             / len(row[self.name+'_h_lag'])
-                turnover_l = len([i for i in row[self.name+'_l_lag'] if i not in row[self.name+'_l']])\
-                             / len(row[self.name+'_l_lag'])
+                turnover_h = len([i for i in row[self.name + '_h_lag'] if i not in row[self.name + '_h']]) \
+                             / len(row[self.name + '_h_lag'])
+                turnover_l = len([i for i in row[self.name + '_l_lag'] if i not in row[self.name + '_l']]) \
+                             / len(row[self.name + '_l_lag'])
                 turnover = (turnover_h + turnover_l) / 2
-                r.at[index, 'turnover_h'], r.at[index, 'turnover_l'], r.at[index, 'turnover']\
-                    =turnover_h, turnover_l, turnover
+                r.at[index, 'turnover_h'], r.at[index, 'turnover_l'], r.at[index, 'turnover'] \
+                    = turnover_h, turnover_l, turnover
             except Exception as e:
                 print(e)
                 r.at[index, 'turnover_h'], r.at[index, 'turnover_l'], r.at[index, 'turnover'] = np.nan, np.nan, np.nan
-        r['port_r_after_fee'] = r['port_r'] - r['turnover']*single_trip_cost*2
+        r['port_r_after_fee'] = r['port_r'] - r['turnover'] * single_trip_cost * 2
         return r
 
-
-    def sort_alpha(self,  model='FF5', mom=True, freq='daily', average_across_2nd_ranks = False):
+    def sort_alpha(self, model='FF5', mom=True, freq='daily', average_across_2nd_ranks=False):
         """
         data: return of sorted portfolio returns per freq(daily, weekly, monthly, annually)
         model: 'FF3', 'FF5', 'q-factor'
         """
-        def reg_OLS(pdf, factor_vars:List[str]):
+
+        def reg_OLS(pdf, factor_vars: List[str]):
             X = sm.add_constant(pdf[factor_vars])
             y = pdf['portr_excess']
-            res = sm.OLS(y, X).fit(cov_type='HAC',cov_kwds={'maxlags':3})
-            res_df= pd.DataFrame({f"coeffs": res.params,
-                                 f'tvalues': res.tvalues,
-                                 f"pvals": res.pvalues
-                                 }, index=X.columns)
+            res = sm.OLS(y, X).fit(cov_type='HAC', cov_kwds={'maxlags': 3})
+            res_df = pd.DataFrame({f"coeffs": res.params,
+                                   f'tvalues': res.tvalues,
+                                   f"pvals": res.pvalues
+                                   }, index=X.columns)
             res_df.index.name = 'vars'
             return res_df
 
@@ -196,14 +209,14 @@ class PortfolioAnalysis:
         returns = self.portr.loc[self.portr['port_r'].notnull()]
         if 'cond_rank' in self.portr.columns:
             if average_across_2nd_ranks is True:
-                returns = returns.drop('cond_rank', axis=1).groupby([self.time, 'sig_rank']).mean().reset_index(level = 1)
+                returns = returns.drop('cond_rank', axis=1).groupby([self.time, 'sig_rank']).mean().reset_index(level=1)
                 group_vars = ['sig_rank']
             else:
                 group_vars = ['cond_rank', 'sig_rank']
         else:
             group_vars = ['sig_rank']
         # match returns with factors
-        date_format = {'daily': '%Y%m%d', 'weekly': '%Y%m%d', 'monthly': '%Y%m', 'quarterly':'%Y%m',  'yearly': '%Y'}
+        date_format = {'daily': '%Y%m%d', 'weekly': '%Y%m%d', 'monthly': '%Y%m', 'quarterly': '%Y%m', 'yearly': '%Y'}
         returns.index = returns.index.strftime(date_format[freq])
         df = returns.merge(factors, left_index=True, right_index=True, how='inner')
         df['portr_excess'] = df['port_r'] - df['RF']
@@ -259,4 +272,3 @@ class PortfolioAnalysis:
         return pdf.groupby(group_vars) \
             .apply(gen_perf) \
             .droplevel('filler')
-
