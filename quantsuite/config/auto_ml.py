d17ba623f6f05d61ac4c97fc9f57de33adbe1831
Zhan Li <zhan.li@aol.com>
2021-12-30 11:10:19 -0600

    config for future auto_ml dev

---
+++ quantsuite/config/auto_ml.py
@@ -0,0 +1,14 @@
+import numpy as np
+from ray import tune
+from sklearn.ensemble import *
+auto_ml_config = {
+    'random_forest_regressor': {
+        'params': {'n_estimators': tune.qrandint(50, 150, 50),
+                   'max_features': tune.quniform(0.05, 1.0, 0.05),
+                   'min_samples_split': tune.randint(2, 21),
+                   'min_samples_leaf': tune.randint(1, 21),
+                   'bootstrap': tune.choice([True, False])
+                   },
+        'model': RandomForestRegressor()
+    }
+}
