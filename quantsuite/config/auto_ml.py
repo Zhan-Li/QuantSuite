aa6db1450e93099e3f5f6a95edf17f1039ff030c
Zhan Li <zhan.li@aol.com>
2022-01-01 11:25:57 -0600

    add weightedAverage model config

--- quantsuite/config/auto_ml.py
+++ quantsuite/config/auto_ml.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ray import tune
 from sklearn.ensemble import *
+from quantsuite.forcaster.model import WeightedAverage
 auto_ml_config = {
     'random_forest_regressor': {
         'params': {'n_estimators': tune.qrandint(50, 150, 50),
@@ -10,5 +11,10 @@ auto_ml_config = {
                    'bootstrap': tune.choice([True, False])
                    },
         'model': RandomForestRegressor()
-    }
+    },
+
+    'weighted_average_ic':{
+        'params': {'thresh': tune.quniform(0.001, 0.02, 0.001)
+                   },
+        'model': WeightedAverage(weight='IC')}
 }
