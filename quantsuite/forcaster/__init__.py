2419112da09b401dfec0282b04d18cac1fe1548a
Zhan Li <zhan.li@aol.com>
2022-02-10 13:28:48 -0600

    Update __init__.py

--- quantsuite/forcaster/__init__.py
+++ quantsuite/forcaster/__init__.py
@@ -1,6 +1,6 @@
 from .forcaster import Forecaster
 from .forcaster import TrainTestSplitter
-from .forcaster import CustomCV
+from .forcaster import CustomCV, TensorFlowForcaster
 from .pipe import Pipe
 from .transformers import Winsorizer, Identity
 from .model import WeightedAverage
