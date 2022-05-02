12d83438d9d4ab273696d995b5186fae4161d8e0
Zhan Li <zhan.li@aol.com>
2022-05-01 23:18:48 -0500

    add target encoder

--- quantsuite/forcaster/transformers.py
+++ quantsuite/forcaster/transformers.py
@@ -1,4 +1,7 @@
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import KFold
 
 
 class Winsorizer(TransformerMixin):
@@ -31,3 +34,332 @@ class Identity(TransformerMixin):
 
     def inverse_transform(self, X, y=None, **fit_params):
         return X
+
+
+class TargetEncoder(BaseEstimator, TransformerMixin):
+    """Target encoder.
+
+    Replaces categorical column(s) with the mean target value for
+    each category.
+
+    """
+
+    def __init__(self, cols=None):
+        """Target encoder
+
+        Parameters
+        ----------
+        cols : list of str
+            Columns to target encode.  Default is to target
+            encode all categorical columns in the DataFrame.
+        """
+        if isinstance(cols, str):
+            self.cols = [cols]
+        else:
+            self.cols = cols
+
+    def fit(self, X, y):
+        """Fit target encoder to X and y
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : encoder
+            Returns self.
+        """
+
+        # Encode all categorical cols by default
+        if self.cols is None:
+            self.cols = [col for col in X
+                         if str(X[col].dtype) == 'object']
+
+        # Check columns are in X
+        for col in self.cols:
+            if col not in X:
+                raise ValueError('Column \'' + col + '\' not in X')
+
+        # Encode each element of each column
+        self.maps = dict()  # dict to store map for each column
+        for col in self.cols:
+            tmap = dict()
+            uniques = X[col].unique()
+            for unique in uniques:
+                tmap[unique] = y[X[col] == unique].mean()
+            self.maps[col] = tmap
+
+        return self
+
+    def transform(self, X, y=None):
+        """Perform the target encoding transformation.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        Xo = X.copy()
+        for col, tmap in self.maps.items():
+            vals = np.full(X.shape[0], np.nan)
+            for val, mean_target in tmap.items():
+                vals[X[col] == val] = mean_target
+            Xo[col] = vals
+        return Xo
+
+    def fit_transform(self, X, y=None):
+        """Fit and transform the data via target encoding.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values (required!).
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        return self.fit(X, y).transform(X, y)
+
+
+class TargetEncoderCV(TargetEncoder):
+    """Cross-fold target encoder.
+    """
+
+    def __init__(self, n_splits=3, shuffle=True, cols=None):
+        """Cross-fold target encoding for categorical features.
+
+        Parameters
+        ----------
+        n_splits : int
+            Number of cross-fold splits. Default = 3.
+        shuffle : bool
+            Whether to shuffle the data when splitting into folds.
+        cols : list of str
+            Columns to target encode.
+        """
+        self.n_splits = n_splits
+        self.shuffle = shuffle
+        self.cols = cols
+
+    def fit(self, X, y):
+        """Fit cross-fold target encoder to X and y
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : encoder
+            Returns self.
+        """
+        self._target_encoder = TargetEncoder(cols=self.cols)
+        self._target_encoder.fit(X, y)
+        return self
+
+    def transform(self, X, y=None):
+        """Perform the target encoding transformation.
+
+        Uses cross-fold target encoding for the training fold,
+        and uses normal target encoding for the test fold.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+
+        # Use target encoding from fit() if this is test data
+        if y is None:
+            return self._target_encoder.transform(X)
+
+        # Compute means for each fold
+        self._train_ix = []
+        self._test_ix = []
+        self._fit_tes = []
+        kf = KFold(n_splits=self.n_splits, shuffle=self.shuffle)
+        for train_ix, test_ix in kf.split(X):
+            self._train_ix.append(train_ix)
+            self._test_ix.append(test_ix)
+            te = TargetEncoder(cols=self.cols)
+            if isinstance(X, pd.DataFrame):
+                self._fit_tes.append(te.fit(X.iloc[train_ix, :],
+                                            y.iloc[train_ix]))
+            elif isinstance(X, np.ndarray):
+                self._fit_tes.append(te.fit(X[train_ix, :],
+                                            y[train_ix]))
+            else:
+                raise TypeError('X must be DataFrame or ndarray')
+
+        # Apply means across folds
+        Xo = X.copy()
+        for ix in range(len(self._test_ix)):
+            test_ix = self._test_ix[ix]
+            if isinstance(X, pd.DataFrame):
+                Xo.iloc[test_ix, :] = \
+                    self._fit_tes[ix].transform(X.iloc[test_ix, :])
+            elif isinstance(X, np.ndarray):
+                Xo[test_ix, :] = \
+                    self._fit_tes[ix].transform(X[test_ix, :])
+            else:
+                raise TypeError('X must be DataFrame or ndarray')
+        return Xo
+
+    def fit_transform(self, X, y=None):
+        """Fit and transform the data via target encoding.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values (required!).
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        return self.fit(X, y).transform(X, y)
+
+
+class TargetEncoderLOO(TargetEncoder):
+    """Leave-one-out target encoder.
+    """
+
+    def __init__(self, cols=None):
+        """Leave-one-out target encoding for categorical features.
+
+        Parameters
+        ----------
+        cols : list of str
+            Columns to target encode.
+        """
+        self.cols = cols
+
+    def fit(self, X, y):
+        """Fit leave-one-out target encoder to X and y
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to target encode
+        y : pandas Series, shape = [n_samples]
+            Target values.
+
+        Returns
+        -------
+        self : encoder
+            Returns self.
+        """
+
+        # Encode all categorical cols by default
+        if self.cols is None:
+            self.cols = [col for col in X
+                         if str(X[col].dtype) == 'object']
+
+        # Check columns are in X
+        for col in self.cols:
+            if col not in X:
+                raise ValueError('Column \'' + col + '\' not in X')
+
+        # Encode each element of each column
+        self.sum_count = dict()
+        for col in self.cols:
+            self.sum_count[col] = dict()
+            uniques = X[col].unique()
+            for unique in uniques:
+                ix = X[col] == unique
+                self.sum_count[col][unique] = \
+                    (y[ix].sum(), ix.sum())
+
+        # Return the fit object
+        return self
+
+    def transform(self, X, y=None):
+        """Perform the target encoding transformation.
+
+        Uses leave-one-out target encoding for the training fold,
+        and uses normal target encoding for the test fold.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+
+        # Create output dataframe
+        Xo = X.copy()
+
+        # Use normal target encoding if this is test data
+        if y is None:
+            for col in self.sum_count:
+                vals = np.full(X.shape[0], np.nan)
+                for cat, sum_count in self.sum_count[col].items():
+                    vals[X[col] == cat] = sum_count[0] / sum_count[1]
+                Xo[col] = vals
+
+        # LOO target encode each column
+        else:
+            for col in self.sum_count:
+                vals = np.full(X.shape[0], np.nan)
+                for cat, sum_count in self.sum_count[col].items():
+                    ix = X[col] == cat
+                    vals[ix] = (sum_count[0] - y[ix]) / (sum_count[1] - 1)
+                Xo[col] = vals
+
+        # Return encoded DataFrame
+        return Xo
+
+    def fit_transform(self, X, y=None):
+        """Fit and transform the data via target encoding.
+
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values (required!).
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        return self.fit(X, y).transform(X, y)
+
+if __name__ == '__main__':
+    import pandas as pd
+    import numpy as np
+    import string
+    data = pd.DataFrame({
+        'a': np.random.randint(1, 100, size=9),
+        'b': np.random.choice(['k', 't', 'q'], size=9)
+    })
+    data['c'] = TargetEncoderCV(3, False, cols=['b']).fit_transform(X = data[['b']], y=data[['a']])
