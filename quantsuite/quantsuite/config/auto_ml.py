a1e0c28db70c489f325df7605df8cea62d051b96
Zhan Li <zhan.li@aol.com>
2022-06-14 10:21:03 -0500

    update random forest hyper parameter search space

--- quantsuite/quantsuite/config/auto_ml.py
+++ quantsuite/quantsuite/config/auto_ml.py
@@ -6,13 +6,12 @@ from xgboost.sklearn import XGBRegressor
 
 auto_ml_config = {
     'random_forest_regressor': {
-        'params': {'n_estimators': tune.qrandint(50, 150, 50),
-                   'max_features': tune.quniform(0.05, 1.0, 0.05),
-                   'min_samples_split': tune.randint(2, 21),
-                   'min_samples_leaf': tune.randint(1, 21),
-                   'bootstrap': tune.choice([True, False])
+        'params': {
+                    'criterion': tune.choice(['squared_error', 'absolute_error']),
+                   'max_features': tune.quniform(0.1, 1.0, 0.1),
+                    'max_samples': tune.quniform(0.1, 1.0, 0.1)
                    },
-        'model': RandomForestRegressor()
+        'model': RandomForestRegressor(n_estimators = 150, random_state=0)
     },
 
     'weighted_average': {
